1caaafb267ed1523bb8158c2263cdba0268bddff
Arthur <arthur.dennetiere@gmail.com>
2021-01-26 15:22:04 +0100

    Ajout JS pour création de graphiques Chartjs

---
+++ api/data.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * filename: data.php
+ * description: this will return the temperature of the sensor
+ */
+
+//setting header to json
+header('Content-Type: application/json');
+
+//database
+define('DB_HOST', '127.0.0.1');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'password');
+define('DB_NAME', 'weather');
+
+//get connection
+$mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if(!$mysqli){
+  die("Connection failed: " . $mysqli->error);
+}
+
+//query to get data from the table
+$query = sprintf("SELECT heure, temperature FROM test ORDER BY heure");
+
+//execute query
+$result = $mysqli->query($query);
+
+//loop through the returned data
+$data = array();
+foreach ($result as $row) {
+  $data[] = $row;
+}
+
+//free memory associated with result
+$result->close();
+
+//close connection
+$mysqli->close();
+
+//now print the data
+print json_encode($data);
+
+?>
